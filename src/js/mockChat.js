5fd513e5a2c489e62dec8b9236e6727b0c77d59b
Daniel <4803580@alu.murciaeduca.es>
2026-05-14 12:06:29 +0200

    frontend-prototipo

--- src/js/mockChat.js
+++ src/js/mockChat.js
@@ -1,13 +1,13 @@
 const mockResponses = {
 
     horarios:
-        "El horario del centro es de 8:25 a 14:25 (MAÑANA) - de 15:50 a 21:40 (TARDE)" ,
+        "El horario del centro es de 8:25 a 14:25 (MAÑANA) - de 15:50 a 21:40 (TARDE)." ,
 
     becas:
         "Puedes solicitar las becas desde la página oficial del ministerio de educación.",
 
     secretaria:
-        "La secretaría se encuentra en la entrada principal del instituto, a mano izquierda",
+        "La secretaría se encuentra en la entrada principal del instituto, a mano izquierda.",
 
     contacto:
         "Puedes contactar mediante teléfono o correo electrónico oficial del centro ===> Tlf: 968301352 | Correo: 30009010@murciaeduca.es",
